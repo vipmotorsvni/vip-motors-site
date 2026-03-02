fd240160d614e8771a2f5163ee163c38f214c9e7
[vipmotorsvni] <[vipmotorsvni@gmail.com]>
2026-03-02 15:14:27 -0300

    Initial

---
+++ script.js
@@ -0,0 +1,11 @@
+/* ================= script.js ================= */
+
+const ano = document.getElementById("ano")
+
+ano.innerHTML = "© " + new Date().getFullYear() + " Vip Motors"
+
+function abrirWhats(){
+
+window.open("https://wa.me/5528999868820","_blank")
+
+}
